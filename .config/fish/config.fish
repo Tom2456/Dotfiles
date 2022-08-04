acdb6a6015dfb816c6d19c2e6c267f212e2894c3
Tom2456 <tomcatfighterjet@protonmail.com>
2022-08-04 22:47:01 +0200

    adding dots :)

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+set -U fish_greeting ">>>Fish up and ready.<<<" 
+neofetch                                      
+                                      
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_vi_key_bindings
+alias vim nvim 
---
+++ .config/nvim/init.vim
@@ -0,0 +1,5 @@
+#set up nvim to use vim config and runtime paths
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+
